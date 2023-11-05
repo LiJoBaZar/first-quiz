11921b3be2704da31a6ca5902b16aabe462a9488
Johanna Balcazar <106547874+LiJoBaZar@users.noreply.github.com>
2023-11-05 14:45:04 -0500

    Update VendingMachineImpl.java
    Add logic the implementation for VendingMachineImpl

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,64 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+import java.util.HashMap;
+import java.util.Map;
 
-  public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
-  }
+public class VendingMachineImpl implements VendingMachine {
+
+    private static VendingMachineImpl instance;
+
+    private Map<String, Drink> drinks = new HashMap<>();
+    private int balance = 0;
+
+    private VendingMachineImpl() {
+        drinks.put("ScottCola", new DrinkImpl("ScottCola", true));
+        drinks.put("KarenTea", new DrinkImpl("KarenTea", false));
+    }
+
+    public static VendingMachine getInstance() {
+        if (instance == null) {
+            instance = new VendingMachineImpl();
+        }
+        return instance;
+    }
+
+    @Override
+    public void insertQuarter() {
+        balance += 25;
+    }
+
+    @Override
+    public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+        if (!drinks.containsKey(name)) {
+            throw new UnknownDrinkException();
+        }
+
+        Drink selectedDrink = drinks.get(name);
+        if (balance < 75) {
+            throw new NotEnoughMoneyException();
+        }
+
+        balance -= 75;
+        return selectedDrink;
+    }
+
+    private class DrinkImpl implements Drink {
+        private String name;
+        private boolean fizzy;
+
+        public DrinkImpl(String name, boolean fizzy) {
+            this.name = name;
+            this.fizzy = fizzy;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean isFizzy() {
+            return fizzy;
+        }
+    }
 }
